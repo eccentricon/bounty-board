388e7dd470909f15240a849079f83d1256bc185a
Ted Osborne <eccentricon@users.noreply.github.com>
2022-08-13 13:45:24 -0400

    Figure 2.4 Making a variable immutable

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,11 @@
 fun main() {
     println("The hero announces her presence to the world.")
+
+    val heroName: String = "Madrigal"
+    println(heroName)
     var playerLevel: Int = 4
     println(playerLevel)
+
     println("The hero embarks on her journey to locate the enchanted sword.")
     playerLevel += 1
     println(playerLevel)
