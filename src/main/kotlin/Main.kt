b6bd26623e5113ee18234792e9e39f9e5de6b5b0
Ted Osborne <eccentricon@users.noreply.github.com>
2022-08-14 11:23:36 -0400

    Listing 3.3 Checking for more player conditions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,8 +10,16 @@ fun main() {
 
     if (playerLevel == 1) {
         println("Meet Mr. Bubbles in the land of soft things.")
-    } else {
+    } else if (playerLevel <= 5) {
+        println("Save the town from the barbarian invasions.")
+    } else if (playerLevel == 6) {
         println("Locate the enchanted sword.")
+    } else if (playerLevel == 7) {
+        println("Recover the long-lost artifact of creation.")
+    } else if (playerLevel == 8) {
+        println("Defeat Nogartse, bringer of death and eater of worlds.")
+    } else {
+        println("There are no quests right now.")
     }
 
     println("Time passes...")
