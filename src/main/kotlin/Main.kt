47c485fcf6a8a90fb935f1552e96dc4343aaf18d
Ted Osborne <eccentricon@users.noreply.github.com>
2022-08-14 18:47:08 -0400

    Listing 3.10 Refactoring quest with when

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,31 +5,28 @@ fun main() {
     println("The hero announces her presence to the world.")
 
     println(HERO_NAME)
-    var playerLevel = 4
+    var playerLevel = 8
     println(playerLevel)
 
     val hasBefriendedBarbarians = true
     val hasAngeredBarbarians = false
     val playerClass = "paladin"
-    val quest: String = if (playerLevel == 1) {
-        "Meet Mr. Bubbles in the land of soft things."
-    } else if (playerLevel in 2..5) {
-        // Check whether diplomacy is an option
-        val canTalkToBarbarians = !hasAngeredBarbarians &&
-                (hasBefriendedBarbarians || playerClass == "barbarian")
-        if (canTalkToBarbarians) {
-            "Convince the barbarians to call off their invasion."
-        } else {
-            "Save the town from the barbarian invasions."
+    val quest: String = when (playerLevel) {
+        1 -> "Meet Mr. Bubbles in the land of soft things."
+        in 2..5 -> {
+            // Check whether diplomacy is an option
+            val canTalkToBarbarians = !hasAngeredBarbarians &&
+                    (hasBefriendedBarbarians || playerClass == "barbarian")
+            if (canTalkToBarbarians) {
+                "Convince the barbarians to call off their invasion."
+            } else {
+                "Save the town from the barbarian invasions."
+            }
         }
-    } else if (playerLevel == 6) {
-        "Locate the enchanted sword."
-    } else if (playerLevel == 7) {
-        "Recover the long-lost artifact of creation."
-    } else if (playerLevel == 8) {
-        "Defeat Nogartse, bringer of death and eater of worlds."
-    } else {
-        "There are no quests right now."
+        6 -> "Locate the enchanted sword."
+        7 -> "Recover the long-lost artifact of creation."
+        8 -> "Defeat Nogartse, bringer of death and eater of worlds."
+        else -> "There are no quests right now."
     }
     println("The hero approaches the bounty board. It reads:")
     println(quest)
